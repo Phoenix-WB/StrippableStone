057c8b6c7b64bc0101cac791fe8ae802da0c39c5
Phoenix-WB <91941086+Phoenix-WB@users.noreply.github.com>
2021-11-02 20:58:25 +1000

    Uploaded Mod

---
+++ main/java/strippablestone/common/events/PlayerEvents.java
@@ -0,0 +1,38 @@
+package strippablestone.common.events;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import strippablestone.StrippableStone;
+
+@EventBusSubscriber(modid = StrippableStone.MOD_ID, bus = Bus.FORGE)
+public class PlayerEvents {
+	@SubscribeEvent
+	public static void handleStripping(PlayerInteractEvent.RightClickBlock event) {
+		ItemStack pickaxe = event.getItemStack();
+		PlayerEntity player = event.getPlayer();
+		BlockPos blockpos = event.getHitVec().getBlockPos();
+		if (pickaxe.getItem() instanceof PickaxeItem) {
+			BlockState clickedBlock = event.getWorld().getBlockState(blockpos);
+			if(clickedBlock.getBlock() == Blocks.STONE)
+			{
+				event.getWorld().setBlockAndUpdate(blockpos, Blocks.COBBLESTONE.defaultBlockState());
+				player.level.playSound(player, blockpos, SoundEvents.STONE_PLACE, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				if (player != null) {
+					pickaxe.hurtAndBreak(1, player, (p_150686_) -> {
+						p_150686_.broadcastBreakEvent(event.getHand());
+					});
+				}
+			}
+		}
+	}
+}
